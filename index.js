3c9ae19bc26ef3a63b7bfc998775e0708d8c8ea8
Paulo César da Silva <paulocs@bb.com.br>
2018-12-17 13:07:07 -0200

    Apply ESLint

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-'use strict'
+'use strict';
 
 const yaml = require('js-yaml');
 const fs = require('fs');
--- libs/validUtil.js
+++ libs/validUtil.js
@@ -1,20 +1,20 @@
 exports.isGreaterThanZero = (number) => {
-    if (isNaN(number) || null == number) return false;
+    if (isNaN(number) || number === null) return false;
     return number > 0;
 };
 
 exports.isValidEmail = (email) => {
-    if (null == email) return false;
-    const re = /[^\s@]+@[^\s@]+\.[^\s@]+/;
+    if (email === null) return false;
+    const re = /[^\s@]+@[^\s@]+\.[^\s@]+/u;
     return re.test(email);
 };
 
 exports.isValidObjectID = (id) => {
-    const re = /^[0-9a-fA-F]{24}$/;
+    const re = /^[0-9a-fA-F]{24}$/u;
     return re.test(id);
 };
 
 exports.isValidUUID = (uuid) => {
-    const re = /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
+    const re = /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/iu;
     return re.test(uuid);
 };
